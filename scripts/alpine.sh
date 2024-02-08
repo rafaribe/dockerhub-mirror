fded46328d7f73ac4aeb1b31c11192d08f67412e
Rafael Ribeiro <rafael.ntw@gmail.com>
2024-02-08 21:08:41 +0000

    fix: alpine script

--- scripts/alpine.sh
+++ scripts/alpine.sh
@@ -4,12 +4,12 @@
 tags=$(crane ls alpine)
 
 # Filter tags since Go 1.16 and exclude Windows tags using semver cli
-filtered_tags=$(
-  echo "$tags" |
-    grep -E '^[0-9]+\.[0-9]+\.[0-9]+(-.*)?$' | \  # Updated regex to match semantic versioning format
-  grep -Ev 'windowsservercore|nanoserver' | \     # Exclude Windows tags
-  semver -r '>=1.16'
-)
+# Filter the tags for versions greater than or equal to 3.15
+filtered_tags=$(echo "$tags" | grep -E '^[0-9]+\.[0-9]+\.[0-9]+(-.*)?$' | while read -r tag; do
+  if semver -r ">=1.32.0" "$tag" &>/dev/null; then
+    echo "$tag"
+  fi
+done)
 
 # Output the filtered tags
 echo "$filtered_tags"
